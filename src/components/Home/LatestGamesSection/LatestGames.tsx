bec76fdb820822fcb1f9f90d8e1604922ef27b53
Cosmin <ioan.nechita@student.kdg.be>
2025-10-11 09:50:27 +0200

    update game scores and standings

--- src/components/Home/LatestGamesSection/LatestGames.tsx
+++ src/components/Home/LatestGamesSection/LatestGames.tsx
@@ -2,53 +2,52 @@ import { GameCard } from "./GameCard.tsx";
 import type { Game } from "../../../types/Game.ts";
 
 const games: Game[] = [
-    {
-        homeTeam: "Putte SK",
-        homeTeamLogo: "/teams/Putte SK.png",
-        awayTeam: "FC Bucovina Loenhout",
-        awayTeamLogo: "/teams/FC Bucovina Loenhout.png",
-        matchType: "Voetbal Vlaanderen 4P",
-        score: "1-4",
-        date: "2025-09-28",
-        location: "Grensstraat 141 , 2950 Kapellen",
-        time: "15:00"
-    },
-
-    {
-        homeTeam: "FC Bucovina Loenhout",
-        homeTeamLogo: "/teams/FC Bucovina Loenhout.png",
-        awayTeam: "KSOC Maria Ter Heide",
-        awayTeamLogo: "/teams/KSOC Maria Ter Heide.png",
-        matchType: "Voetbal Vlaanderen 4P",
-        score: "3-2",
-        date: "2025-09-21",
-        location: "Bredabaan, 280 , 2990 Wuustwezel",
-        time: "15:00"
-    },
-    {
-        homeTeam: "R. Cappellen FC B",
-        homeTeamLogo: "/teams/R. Cappellen FC B.png",
-        awayTeam: "FC Bucovina Loenhout",
-        awayTeamLogo: "/teams/FC Bucovina Loenhout.png",
-        matchType: "Voetbal Vlaanderen 4P",
-        score: "1-0",
-        date: "2025-09-13",
-        location: "René de Pauwstraat,69 , 2950 Kapellen",
-        time: "18:00"
-    },
+  {
+    homeTeam: "FC Bucovina Loenhout",
+    homeTeamLogo: "/teams/FC Bucovina Loenhout.png",
+    awayTeam: "KSV Wildert",
+    awayTeamLogo: "/teams/KSV Wildert.png",
+    matchType: "Voetbal Vlaanderen 4P",
+    score: "1-2",
+    date: "2025-10-05",
+    location: "Bredabaan, 280 , 2990 Wuustwezel",
+    time: "15:00",
+  },
+  {
+    homeTeam: "Putte SK",
+    homeTeamLogo: "/teams/Putte SK.png",
+    awayTeam: "FC Bucovina Loenhout",
+    awayTeamLogo: "/teams/FC Bucovina Loenhout.png",
+    matchType: "Voetbal Vlaanderen 4P",
+    score: "1-4",
+    date: "2025-09-28",
+    location: "Grensstraat 141 , 2950 Kapellen",
+    time: "15:00",
+  },
+  {
+    homeTeam: "FC Bucovina Loenhout",
+    homeTeamLogo: "/teams/FC Bucovina Loenhout.png",
+    awayTeam: "KSOC Maria Ter Heide",
+    awayTeamLogo: "/teams/KSOC Maria Ter Heide.png",
+    matchType: "Voetbal Vlaanderen 4P",
+    score: "3-2",
+    date: "2025-09-21",
+    location: "Bredabaan, 280 , 2990 Wuustwezel",
+    time: "15:00",
+  },
 ];
 
 export function LatestGames() {
-    return (
-        <div className="flex flex-col items-center justify-center w-full">
-            <div className="flex flex-col w-full lg:w-2/3 mt-4 px-4 sm:px-6 mb-7">
-                <h2 className="text-4xl sm:text-6xl text-center text-green-700 font-bold italic uppercase">
-                    ULTIMELE MECIURI
-                </h2>
-                {games.map((game, index) => (
-                    <GameCard key={index} game={game} />
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex flex-col items-center justify-center w-full">
+      <div className="flex flex-col w-full lg:w-2/3 mt-4 px-4 sm:px-6 mb-7">
+        <h2 className="text-4xl sm:text-6xl text-center text-green-700 font-bold italic uppercase">
+          ULTIMELE MECIURI
+        </h2>
+        {games.map((game, index) => (
+          <GameCard key={index} game={game} />
+        ))}
+      </div>
+    </div>
+  );
 }
